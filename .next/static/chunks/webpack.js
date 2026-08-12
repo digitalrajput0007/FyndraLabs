4c8590336c913a31e355e3c5ae85f186356c0b53
Sachin Rajput <digitalrajput007@gmail.com>
2026-08-12 14:46:56 +0530

    Fixed authorization error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1597c7189de8904f"; }
+/******/ 		__webpack_require__.h = function() { return "fe503a6f9efc8339"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- lib/firebaseClient.ts
+++ lib/firebaseClient.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSy_dummy_build_key",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "splitmate.firebaseapp.com",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB2fCMYGgRHXYOiiESoQeqg9F_BIS2pS60",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "splitmate-d2d66.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "splitmate-d2d66",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "splitmate.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "0000000000",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:0000000000:web:0000000000",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "splitmate-d2d66.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "563027170263",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:563027170263:android:4b72f0d1051e81d406f192",
 };
 
 export const getClientAuth = () => {
